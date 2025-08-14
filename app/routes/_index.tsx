bfdd4cf20a52240128db709169e129a31808f70d
blitzlug <Blitzlug@protonmail.com>
2025-08-14 15:25:22 +0800

    Revert "restructured file structure"
    This reverts commit d5153cb55e521746f6ca25b46363fe04fc0aa589.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,4 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { Header } from "~/components/ui/header";
-import { db } from "~/utils/db.server";
-export async function loader() {
-  const categoryGroups = await db.categoryGroup.findMany({
-    include: {
-      categories: true,
-    },
-  });
-  return { categoryGroups };
-}
 export const meta: MetaFunction = () => {
   return [
     { title: "Beauty Guide" },
@@ -18,25 +7,5 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const { categoryGroups } = useLoaderData<typeof loader>();
-  const handleFacebookLogin = () => {
-    FB.login(
-      (response) => {
-        if (response.authResponse) {
-          console.log("Logged in!", response);
-        } else {
-          console.log("User cancelled login or did not fully authorize.");
-        }
-      },
-      { scope: "public_profile,email" }
-    );
-  };
-  return (
-    <div>
-      <Header
-        categoryGroups={categoryGroups}
-        handleFacebookLogin={handleFacebookLogin}
-      />
-    </div>
-  );
+  return <div>Index</div>;
 }
--- app/routes/admin.advice._index.tsx
+++ app/routes/admin.advice._index.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div>Advice</div>;
+  return <div>Hello </div>;
 }
